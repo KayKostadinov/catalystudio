27ed85149ba989e573e7eba8ff7ffa9be2e23523
Kay Kostadinov <KayKostadinov@users.noreply.github.com>
2020-11-26 21:21:16 +0200

    Resolved POST bug where the server wouldn't read JSON

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,9 +7,9 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 /**
- * @route POST api/users
- * @desc register user
- * @access public
+ * @route   POST api/users
+ * @desc    register user
+ * @access  public
  */
 
 router.post('/', [
@@ -17,55 +17,54 @@ router.post('/', [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'A valid e-mail address is required').isEmail(),
     check('password', 'Password needs to be 6 or more characters').isLength({ min: 6 })
-],
-    async (req, res) => {
-        // check validation and stop execution if errors
-        const error = validationResult(req);
-        if (!error.isEmpty()) {
-            return res.status(400).json({ errors: error.array() });
+], async (req, res) => {
+    // check validation and stop execution if errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+        // check if user exists
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
         }
 
-        const { name, email, password } = req.body;
+        // create Mongo object
+        user = new User({
+            name,
+            email,
+            password
+        })
 
-        try {
-            // check if user exists
-            let user = User.findOne({ email });
-            if (user) {
-                return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        // encrypt pass
+        salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        // save user
+        await user.save();
+
+        // JWT
+        const payload = {
+            user: {
+                id: user.id
             }
-
-            // create Mongo object
-            user = new User({
-                name,
-                email,
-                password
-            })
-
-            // encrypt pass
-            salt = await bcrypt.genSalt(10);
-            user.password = await bcrypt.hash(password, salt);
-
-            // save user
-            await user.save();
-
-            // JWT
-            const payload = {
-                user: {
-                    id: user.id
-                }
-            }
-
-            jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 3600 }, (err, token) => {
-                if (err) {
-                    throw err;
-                }
-
-                res.json({ token });
-            })
-        } catch (err) {
-            console.error(err.message);
-            res.status(500).send('Server Error');
         }
-    })
+
+        jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 3600 }, (err, token) => {
+            if (err) {
+                throw err;
+            }
+
+            res.json({ token });
+        })
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const { json } = require('express');
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json({ extended: false }))
 
 // connect Mongo
 const connectDB = require('./config/db');
@@ -10,6 +13,8 @@ connectDB();
 app.get('/', (req, res) => {
     res.send('Howdy')
 })
+
+app.use('/api/users', require('./routes/api/users'));
 
 // Run server
 app.listen(PORT, () => {
